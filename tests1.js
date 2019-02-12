ce0bee3997c77482b2b792b68e28d59fae392b61
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-02-11 18:47:43 -0600

    etts

--- tests1.js
+++ tests1.js
@@ -1,4 +1,4 @@
 QUnit.test( "TestCube", function( assert ) {
             var result = cube(3);
-            assert.equal( result, "28", "cube(3) should be 27." );
+            assert.equal( result, "27", "cube(3) should be 27." );
          });
